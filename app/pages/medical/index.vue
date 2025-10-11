4b261a42910dddee719ca5828133ba53f6db8e7e
Kybueno <kybueno@github.com>
2025-10-11 13:41:31 -0400

    add:medical

--- app/pages/medical/index.vue
+++ app/pages/medical/index.vue
@@ -1,2 +1,23 @@
+<script setup lang="ts">
+const data = ref([
+  {
+    no_exp: '4600',
+    name:'Joseito Perez',
+    área: 'GAT',
+    desde: '10/10/2025',
+    hasta:'20/10/2025',
+    días_disponibles: '14',
+    incorporacion:'22/10/25',
+    enfermedad:'',
+    inicial_cont:'',
+    días_prenómina:'',
+  },
+])
+</script>
+
 <template>
-    </template>
+  <UTable :data="data" class="flex-1" />
+  <div>
+  <UButton>Añadir</UButton>
+  </div>
+</template>
