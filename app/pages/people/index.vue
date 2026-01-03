33fe07a24b2356783c808e85450b594a0a04800f
Ing. Carlos Enrique <44654304+carlosepcc@users.noreply.github.com>
2026-01-02 21:30:48 -0500

    feat: add navbar to workers page

--- app/pages/people/index.vue
+++ app/pages/people/index.vue
@@ -2,17 +2,29 @@
 import { useWorkerStore } from '~/stores/workerStore'
 
 const workerStore = useWorkerStore()
-
+const searchTerm = ref('')
 onMounted(() => workerStore.loadWorkers())
 
 </script>
 <template>
-    <div class="flex flex-col gap-8 p-4 w-full">
-        <div class="flex justify-between">
+    <UDashboardPanel id="workers">
+        <template #header>
+            <UDashboardNavbar title="Trabajadores">
+             
+                <template #leading>
+                    <UDashboardSidebarCollapse />
+                </template>
+                <UInput v-model="searchTerm" class="max-w-lg" icon="i-lucide-search" placeholder="Buscar trabajadores..." />
+                
+                <template #right>
+                    <UButton icon="i-lucide-plus" to="people/new">Nuevo trabajador</UButton>
+                    <UButton variant="ghost" color="neutral" icon="i-lucide-refresh-cw" />
+                </template>
+            </UDashboardNavbar>
+        </template>
 
-        </div>
-        <div class="overflow-auto max-w-6xl">
+        <template #body>
             <WorkerTable :data="workerStore.workers" />
-        </div>
-    </div>
+        </template>
+    </UDashboardPanel>
 </template>
