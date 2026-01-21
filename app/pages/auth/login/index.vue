958d5b9bf8a83524520bd1e8433fa31ada066287
Ing. Carlos Enrique <44654304+carlosepcc@users.noreply.github.com>
2026-01-21 15:57:42 -0500

    fix: login ui container

--- app/pages/auth/login/index.vue
+++ app/pages/auth/login/index.vue
@@ -72,80 +72,77 @@ const loggedUser = useSupabaseUser()
 </script>
 
 <template>
-  <div class=" flex items-center justify-center p-4 mx-auto bg-muted rounded-xl w-full max-w-lg">
-    <UCard v-if="!loggedUser" class="w-full max-w-lg">
-      <Stack class="text-center items-center p-4">
-        <h2 class="text-center text-2xl font-bold">
-          Bienvenido
-        </h2>
-        <p class="max-w-xs mx-auto text-muted">{{ mode === 'signup' ? 'Puede registrarse en el sistema' : 'Inicie su sesión en el sistema' }}</p>
-      </Stack>
-
-      <div class="p-6">
-       
-
-        <!-- Auth Form -->
-        <form @submit.prevent="handleAuth" class="space-y-6">
-          <div class="space-y-4 flex flex-col items-center">
-
-            <Stack>
-              <UFormField label="Nombre de usuario UCI">
-                <UInput size="xl" v-model="username" placeholder="Enter your email" class="w-full" />
-              </UFormField>
-              <UFormField label="Contraseña" :error="errorMessage.toLowerCase().includes('password') && errorMessage">
-                <UInput size="xl" v-model="password" placeholder="Password" :type="showPassword ? 'text' : 'password'"
-                  class="w-full" :ui="{ trailing: 'pe-1' }" required>
-                  <template #trailing>
-                    <UButton color="neutral" variant="link" size="sm"
-                      :icon="showPassword ? 'i-lucide-eye-off' : 'i-lucide-eye'"
-                      :aria-label="showPassword ? 'Hide password' : 'Show password'" :aria-pressed="showPassword"
-                      aria-controls="password" @click="showPassword = !showPassword" />
-                  </template>
-                </UInput>
-              </UFormField>
-              
-              <UButton class="mt-4 w-full justify-center" size="lg" variant="subtle" :icon="mode==='signup' ? 'mdi:account-plus-outline' : 'mdi:login'" :loading="loading" type="submit" >
-              {{ mode === 'signup' ? 'Registrarme' : 'Entrar' }}
-            </UButton>
-            </Stack>
-
-
-            
-          </div>
-           <!-- Error/Success Messages -->
-        <UAlert v-if="errorMessage" color="error" variant="subtle" title="¡Error!" :description="errorMessage"
-          icon="lucide-user" />
-        <UAlert v-if="successMessage" color="success" variant="subtle" title="¡Éxito!" :description="successMessage"
-          icon="lucide-user" />
-        </form>
-
-
-
-       
-      </div>
-      <template #footer>
-        <div class="py-2 space-y-2 text-muted">
-          <HStack class="justify-center items-center">
-            <p>
-              {{ mode === 'signup' ? '¿Ya tiene una cuenta?' : '¿No tiene una cuenta?' }}
-            </p>
-              <UButton variant="soft" @click="toggleMode">
-                {{ mode === 'signup' ? 'Inicie sesión' : 'Regístrese' }}
+  <Stack class="justify-between h-full">
+    <div>
+      <!-- Just a spacer -->
+    </div>
+    <div class=" flex items-center justify-center p-4 mx-auto bg-muted rounded-xl w-full max-w-lg">
+      <UCard v-if="!loggedUser" class="w-full max-w-lg">
+        <Stack class="text-center items-center p-4">
+          <h2 class="text-center text-2xl font-bold">
+            Bienvenido
+          </h2>
+          <p class="max-w-xs mx-auto text-muted">{{ mode === 'signup' ? 'Puede registrarse en el sistema' : 'Inicie su sesión en el sistema' }}</p>
+        </Stack>
+        <div class="p-6">
+    
+          <!-- Auth Form -->
+          <form @submit.prevent="handleAuth" class="space-y-6">
+            <div class="space-y-4 flex flex-col items-center">
+              <Stack>
+                <UFormField label="Nombre de usuario" hint="@uci.cu" >
+                  <UInput size="xl" v-model="username" placeholder="Enter your email" class="w-full" />
+                </UFormField>
+                <UFormField label="Contraseña" :error="errorMessage.toLowerCase().includes('password') && errorMessage">
+                  <UInput size="xl" v-model="password" placeholder="Password" :type="showPassword ? 'text' : 'password'"
+                    class="w-full" :ui="{ trailing: 'pe-1' }" required>
+                    <template #trailing>
+                      <UButton color="neutral" variant="link" size="sm"
+                        :icon="showPassword ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+                        :aria-label="showPassword ? 'Hide password' : 'Show password'" :aria-pressed="showPassword"
+                        aria-controls="password" @click="showPassword = !showPassword" />
+                    </template>
+                  </UInput>
+                </UFormField>
+    
+                <UButton class="mt-4 w-full justify-center" size="lg" variant="subtle" :icon="mode==='signup' ? 'mdi:account-plus-outline' : 'mdi:login'" :loading="loading" type="submit" >
+                {{ mode === 'signup' ? 'Registrarme' : 'Entrar' }}
               </UButton>
-          </HStack>
-          <Box class="text-center">
-              <p>¿Problemas para acceder?</p>
-              <a href="mailto:" class="text-primary hover:underline whitespace-nowrap">Contacte a Recursos Humanos</a>
-          </Box>
+              </Stack>
+    
+            </div>
+             <!-- Error/Success Messages -->
+          <UAlert v-if="errorMessage" color="error" variant="subtle" title="¡Error!" :description="errorMessage"
+            icon="lucide-user" />
+          <UAlert v-if="successMessage" color="success" variant="subtle" title="¡Éxito!" :description="successMessage"
+            icon="lucide-user" />
+          </form>
+    
         </div>
+        <template #footer>
+          <div class="py-5 space-y-6 text-muted">
+            <HStack class="justify-center items-center">
+              <p>
+                {{ mode === 'signup' ? '¿Ya tiene una cuenta?' : '¿No tiene una cuenta?' }}
+              </p>
+                <UButton variant="soft" @click="toggleMode">
+                  {{ mode === 'signup' ? 'Inicie sesión' : 'Regístrese' }}
+                </UButton>
+            </HStack>
+          </div>
+        </template>
+      </UCard>
+      <template v-else>
+        <NuxtLink href="/dashboard">
+          <Button>Ir al Panel de Información</Button>
+        </NuxtLink>
       </template>
-    </UCard>
-    <template v-else>
-      <NuxtLink href="/dashboard">
-        <Button>Ir al Panel de Información</Button>
-      </NuxtLink>
-    </template>
-  </div>
+    </div>
+      <Box class="text-center text-muted">
+                <p>¿Problemas para acceder?</p>
+                <a href="mailto:" class="text-primary hover:underline whitespace-nowrap">Contacte a Recursos Humanos</a>
+            </Box>
+  </Stack>
 </template>
 
 <style scoped>
