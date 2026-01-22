a7afcc658a5ebbb47df3e8639b6d82deab55b1f0
Ing. Carlos Enrique <44654304+carlosepcc@users.noreply.github.com>
2026-01-22 00:49:08 -0500

    fix: remove dev info

--- app/pages/people/index.vue
+++ app/pages/people/index.vue
@@ -9,7 +9,7 @@ useHead({
 })
 
 const searchTerm = ref('')
-useAsyncData('workers', loadWorkers)
+
 onMounted(() => { if (!workersPending.value && !workers.value.length) loadWorkers() })
 
 function handleLoadWorkers() {
@@ -22,7 +22,6 @@ const {loggedUserProfile:user} = useAuthStore()
     <div class="space-y-8 mt-8">
         <div class="flex justify-between">
             <UInput v-model="searchTerm" class="max-w-lg" icon="i-lucide-search" placeholder="Buscar trabajadores..." />
-{{ user?.roles }}
             <div class="flex gap-3">
                 <UButton v-if="user?.roles?.some((r)=>(['director','hr_manager'].includes(r)))" icon="i-lucide-plus" to="people/new" variant="ghost">Nuevo trabajador</UButton>
                 <UButton @click="handleLoadWorkers" variant="ghost" icon="i-lucide-refresh-cw"
