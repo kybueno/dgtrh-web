b38b840f816364941727bb4a822f0caee2e6673d
Ing. Carlos Enrique <44654304+carlosepcc@users.noreply.github.com>
2026-02-04 23:11:43 -0500

    feat: unique code migration

---
+++ prisma/migrations/20260205035846_organization_code_unique/migration.sql
@@ -0,0 +1,196 @@
+-- CreateEnum
+CREATE TYPE "worker_status" AS ENUM ('active', 'inactive', 'terminated');
+
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('hr_manager', 'director', 'employee');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'employee',
+    "worker_id" UUID,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "workers" (
+    "id" UUID NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "middle_name" TEXT,
+    "last_name" TEXT NOT NULL,
+    "second_last_name" TEXT NOT NULL,
+    "record_number" TEXT NOT NULL,
+    "ci" TEXT,
+    "email" TEXT,
+    "level" TEXT,
+    "address" TEXT,
+    "gender" TEXT,
+    "tel" TEXT,
+    "organizations_codes" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "parent1_name" TEXT,
+    "parent2_name" TEXT,
+    "status" "worker_status" NOT NULL DEFAULT 'active',
+    "group_id" INTEGER,
+    "position_code" INTEGER,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "workers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "groups" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "leader_id" UUID NOT NULL,
+
+    CONSTRAINT "groups_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "positions" (
+    "id" SERIAL NOT NULL,
+    "code" INTEGER NOT NULL,
+    "category" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "group_escale" TEXT NOT NULL,
+    "level" TEXT NOT NULL,
+    "scale" INTEGER,
+    "total" INTEGER,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "positions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "organizations" (
+    "id" SERIAL NOT NULL,
+    "acronym" TEXT,
+    "code" TEXT NOT NULL,
+    "description" TEXT,
+    "img" TEXT,
+    "name" TEXT,
+
+    CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "extra_work" (
+    "id" UUID NOT NULL,
+    "worker_id" UUID NOT NULL,
+    "start_date" TIMESTAMP(3) NOT NULL,
+    "end_date" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "extra_work_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "incident_types" (
+    "code" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "classification" TEXT NOT NULL,
+    "description" TEXT,
+    "pay_for" TEXT,
+    "time_classification" TEXT,
+    "deductible" BOOLEAN,
+    "incentive" BOOLEAN NOT NULL DEFAULT false,
+    "avg" BOOLEAN NOT NULL DEFAULT false,
+    "sum" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "incident_types_pkey" PRIMARY KEY ("code")
+);
+
+-- CreateTable
+CREATE TABLE "incidents" (
+    "id" UUID NOT NULL,
+    "worker_id" UUID NOT NULL,
+    "incident_code" INTEGER NOT NULL,
+    "description" TEXT,
+    "start_date" TIMESTAMP(3) NOT NULL,
+    "end_date" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "incidents_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "holidays" (
+    "id" UUID NOT NULL,
+    "worker_id" UUID,
+    "desde" TIMESTAMP(3),
+    "hasta" TIMESTAMP(3),
+    "incorporacion" TIMESTAMP(3),
+    "motivo" TEXT,
+    "tiempo" JSONB,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "holidays_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "payroll" (
+    "id" SERIAL NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "status" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" UUID,
+    "revised_by" UUID,
+
+    CONSTRAINT "payroll_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_worker_id_key" ON "users"("worker_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "workers_email_key" ON "workers"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "positions_code_key" ON "positions"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organizations_code_key" ON "organizations"("code");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_worker_id_fkey" FOREIGN KEY ("worker_id") REFERENCES "workers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "workers" ADD CONSTRAINT "workers_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "groups"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "workers" ADD CONSTRAINT "workers_position_code_fkey" FOREIGN KEY ("position_code") REFERENCES "positions"("code") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "groups" ADD CONSTRAINT "groups_leader_id_fkey" FOREIGN KEY ("leader_id") REFERENCES "workers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "extra_work" ADD CONSTRAINT "extra_work_worker_id_fkey" FOREIGN KEY ("worker_id") REFERENCES "workers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "incidents" ADD CONSTRAINT "incidents_worker_id_fkey" FOREIGN KEY ("worker_id") REFERENCES "workers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "incidents" ADD CONSTRAINT "incidents_incident_code_fkey" FOREIGN KEY ("incident_code") REFERENCES "incident_types"("code") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "holidays" ADD CONSTRAINT "holidays_worker_id_fkey" FOREIGN KEY ("worker_id") REFERENCES "workers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payroll" ADD CONSTRAINT "payroll_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "workers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payroll" ADD CONSTRAINT "payroll_revised_by_fkey" FOREIGN KEY ("revised_by") REFERENCES "workers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
